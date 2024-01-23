59b947792e2f472487e98ac3003ead6d71df5666
shalinipatel1621 <shalinipatel1621@gmail.com>
2024-01-23 07:19:21 +0000

    gorest

---
+++ src/main/java/com/gorest/constants/EndPoints.java
@@ -0,0 +1,15 @@
+package com.gorest.constants;
+
+
+public class EndPoints {
+
+    public static final String GET_ALL_USERS = "/users";
+    public static final String GET_SINGLE_USER_BY_ID = "/users/{userID}";
+    public static final String UPDATE_USER_BY_ID = "/users/{userID}";
+    public static final String DELETE_USER_BY_ID = "/users/{userID}";
+}
+
+
+
+
+
---
+++ src/main/java/com/gorest/constants/Path.java
@@ -0,0 +1,20 @@
+package com.gorest.constants;
+
+/**
+ * Created by Jay
+ */
+public class Path {
+
+    /*
+     * This is base path for student api
+     */
+    public static final String USERS = "/public/v2";
+
+    /*
+     * This is base path for authenticate api
+     */
+    /*public static final String AUTHENTICATE = "/authenticate";
+
+    public static final String ACCOUNT = "/account";*/
+
+}
